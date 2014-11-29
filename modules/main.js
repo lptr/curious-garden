bf3b8df55c0e532025798e523cfb4bc927666584
Lorant Pinter <lorant.pinter@prezi.com>
2014-11-29 15:49:51 +0100

    Modularizing

--- modules/main.js
+++ modules/main.js
@@ -1,17 +1,13 @@
-var queryServer = function(method, data, callback) {
-	$.ajax("https://script.google.com/macros/s/AKfycbzr__tdtQoB4Hg9mJOCaK8Y9AL8Fwx4uyIxHefJSis/dev",
-	{
-		dataType: "jsonp",
-		data: {
-			method: method,
-			data: JSON.stringify(data)
-		},
-		success: function(data) {
-			callback(data);
-		},
-		error: function (xhr, error, exception) {
-			console.log("Error", xhr, error, exception);
-			alert("Error: " + error);
-		}
+(function () {
+	var app = angular.module("kapa", ["kapa.transactions", "ngRoute"]);
+//	app.config(function ($locationProvider) {
+//		$locationProvider.html5Mode(true);
+//	});
+
+	app.config(function ($routeProvider) {
+		$routeProvider
+			.when("/", {
+				templateUrl: "modules/main.html"
+			});
 	});
-}
+})();
