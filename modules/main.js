aeb3d3176c9ec0451f34cabdd658ed5d99ead4b5
Lorant Pinter <lorant.pinter@prezi.com>
2014-11-29 19:58:30 +0100

    Clearable inputs

--- modules/main.js
+++ modules/main.js
@@ -10,4 +10,17 @@
 				templateUrl: "modules/main.html"
 			});
 	});
+
+	$(function () {
+		function tog(v) {
+			return v ? 'addClass' : 'removeClass';
+		} 
+		$(document).on('input', '.clearable', function() {
+			$(this)[tog(this.value)]('x');
+		}).on('mousemove', '.x', function(e) {
+			$(this)[tog(this.offsetWidth - 18 < e.clientX - this.getBoundingClientRect().left)]('onX');   
+		}).on('click', '.onX', function() {
+			$(this).removeClass('x onX').val('').change();
+		});
+	});	
 })();
