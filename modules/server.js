d114b60a4304a7525659ccce21424d2330ef449b
Lorant Pinter <lorant.pinter@prezi.com>
2014-11-30 01:54:29 +0100

    When ?dev=1, use dev Google Apps Script backend

--- modules/server.js
+++ modules/server.js
@@ -1,9 +1,29 @@
 (function () {
+	var getQueryString = function () {
+		var result = {}
+		var queryString = location.search.slice(1);
+		var re = /([^&=]+)=([^&]*)/g;
+		var m;
+
+		while (m = re.exec(queryString)) {
+			result[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+		}
+
+		return result;
+	}
+
 	var server = angular.module("kapa.server", []);
-	server.factory("kapaServer", function ($http) {
+	server.factory("kapaServer", function ($http, $location) {
+		var development = getQueryString()["dev"];
+		var serverUrl;
+		if (development) {
+			serverUrl = "https://script.google.com/macros/s/AKfycbzr__tdtQoB4Hg9mJOCaK8Y9AL8Fwx4uyIxHefJSis/dev";
+		} else {
+			serverUrl = "https://script.google.com/macros/s/AKfycbwBGeZPzlmi-4wu23s3NyQP0T1rE71750FNYOFPAozPignicus/exec"
+		}
 		return {
 			query: function (method, data) {
-				return $http.jsonp("https://script.google.com/macros/s/AKfycbzr__tdtQoB4Hg9mJOCaK8Y9AL8Fwx4uyIxHefJSis/dev", {
+				return $http.jsonp(serverUrl, {
 					params: {
 						method: method,
 						data: JSON.stringify(data),
