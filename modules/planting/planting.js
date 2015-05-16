c662700d2a446f48890f5dd0b64bd09a43c025d3
Lorant Pinter <lorant.pinter@prezi.com>
2015-05-17 01:58:10 +0200

    Create a lookup property

--- modules/planting/planting.js
+++ modules/planting/planting.js
@@ -16,10 +16,20 @@
 	});
 
 	transactionModule.controller("PlantingController", function ($scope, kapaServer) {
+		var produces = [
+			{ id: 1, name: "Zsázsa", rowWidth: 15 },
+			{ id: 2, name: "Mizuna", rowWidth: 12 },
+		];
+		var plantations = [
+			{ id: 1, produce: "Zsázsa", seed: "Zsázsamag", time: "2015-05-13", seedsPerGramm: 4 },
+			{ id: 2, produce: "Mizuna", seed: "Mizunamag", time: "2015-05-17", seedsPerGramm: 0.25 },
+		];
+		$scope.items = plantations;
+
 		var afterChange = function (event) {
 			console.log("Event", event);
 		}
-		
+
 		var suffixRenderer = function (suffix) {
 			return function (instance, td, row, col, prop, value, cellProperties) {
 				Handsontable.renderers.NumericRenderer.apply(this, [instance, td, row, col, prop, value, cellProperties]);
@@ -28,6 +38,66 @@
 				}
 			};
 		};
+		
+		var property = function (get, set) {
+			return function (item, value) {
+				if (typeof value === 'undefined') {
+					return get.apply({}, [item]);
+				} else {
+					return set.apply({}, [item, value]);
+				}
+			}
+		}
+		
+		var LookupProperty = function (options) {
+			var nameField = options.name;
+			var idField = options.id || nameField + "Id";
+
+			var lookup = {};
+			options.data.forEach (function (datum) {
+				var id = datum["id"];
+				var name = datum["name"];
+				lookup[name] = datum;
+			});
+
+			this.get = function (item) {
+				return item[nameField];
+			};
+			this.set = function (item, name) {
+				item[nameField] = name;
+				var value = lookup[name];
+				item[idField] = value ? value.id : null;
+			};
+			this.renderer = function (instance, td, row, col, prop, name, cellProperties) {
+				Handsontable.renderers.NumericRenderer.apply(this, [instance, td, row, col, prop, name, cellProperties]);
+				var value = lookup[name];
+				if (!value) {
+					td.color = "red";
+				} else {
+					td.title = "ID: " + value.id;
+				}
+				return td;
+			};
+			this.toColumn = function () {
+				return {
+					type: "dropdown",
+					source: options.data.map(function (datum) { return datum.name; }),
+					title: options.title,
+					data: property(this.get, this.set),
+					renderer: this.renderer
+				};
+			};
+		}
+		
+		var produceProp = new LookupProperty({ name: "produce", title: "Termény", data: produces });
+
+		var columns = [
+			{ type: "text", title: "ID", data: "id", readOnly: true },
+			produceProp.toColumn(),
+			{ type: "text", title: "Mag", data: "seed", hidden: true },
+			{ type: "date", title: "Dátum", data: "time", dateFormat: "YYYY-MM-DD" },
+			{ type: "numeric", title: "Magok száma", data: "seedsPerGramm", format: "0.00", renderer: suffixRenderer(" db/g")},
+		];
 
 		$scope.settings = {
 			colHeaders: true,
@@ -37,17 +107,7 @@
 			minSpareRows: 1,
 			height: 300,
 			width: 700,
-			columns: [
-				{ type: "text", title: "ID", data: "id", readOnly: true },
-				{ type: "text", title: "Terménynév", data: "name" },
-				{ type: "text", title: "Szín", data: "color" },
-				{ type: "date", title: "Dátum", data: "time", dateFormat: "YYYY-MM-DD" },
-				{ type: "numeric", title: "Magok száma", data: "seedsPerGramm", format: "0.00", renderer: suffixRenderer(" db/g")},
-			]
+			columns: columns.filter(function (column) { return !column.hidden; })
 		};
-		$scope.items = [
-			{ id: 1, name: "Zsázsa", color: "Blue", time: "2015-05-13", seedsPerGramm: 4 },
-			{ id: 2, name: "Mizuna", color: "Zöld", time: "2015-05-17", seedsPerGramm: 0.25 },
-		];
 	});
 })();
