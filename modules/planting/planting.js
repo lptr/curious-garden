c728dd593d4f63532b7b163b06b03ff67d6a9839
Lorant Pinter <lorant.pinter@prezi.com>
2015-05-18 17:25:17 +0200

    Use prototypal inheritence

--- modules/planting/planting.js
+++ modules/planting/planting.js
@@ -29,112 +29,119 @@
 			};
 		};
 		
-		var makeProperty = function (get, set) {
+		var Property = function (options) {
+			$.extend(this, options);
+		};
+		Property.prototype.name = function () {
+			return this.property;
+		};
+		Property.prototype.value = function (item) {
+			return item[this.property];
+		};
+		Property.prototype.toProperty = function () {
+			var self = this;
 			return function (item, value) {
 				if (typeof value === 'undefined') {
-					return get.apply({}, [item]);
+					return self.get(item);
 				} else {
-					return set.apply({}, [item, value]);
+					return self.set(item, value);
 				}
-			}
-		}
+			};
+		};
 		
 		var SimpleProperty = function (options) {
-			var property = options.property;
-			
-			this.name = function () {
-				return property;
-			};
-			this.get = function (item) {
-				return item[property];
-			};
-			this.set = function (item, value) {
-				item[property] = value;
-			}
-			this.toJson = function (item, json) {
-				json[property] = this.get(item);
-			};
-			this.fromJson = function (item, json) {
-				this.set(item, json[property]);
-			};
-			this.toColumn = function () {
-				return $.extend({}, options.column, {
-					type: options.type || "text",
-					title: options.title,
-					data: makeProperty(this.get, this.set),
-					readOnly: options.readOnly ? true : false
-				});
-			};
+			Property.apply(this, arguments);
+		}
+		SimpleProperty.prototype = Object.create(Property.prototype);
+		SimpleProperty.prototype.get = function (item) {
+			return item[this.property];
+		};
+		SimpleProperty.prototype.set = function (item, value) {
+			item[this.property] = value;
+		}
+		SimpleProperty.prototype.toJson = function (item, json) {
+			json[this.property] = this.get(item);
+		};
+		SimpleProperty.prototype.fromJson = function (item, json) {
+			this.set(item, json[this.property]);
+		};
+		SimpleProperty.prototype.toColumn = function () {
+			return $.extend({}, this.column, {
+				type: this.type || "text",
+				title: this.title,
+				data: this.toProperty(),
+				readOnly: this.readOnly ? true : false
+			});
 		};
 
 		var ReferenceProperty = function (options) {
-			var property = options.property;
-			var idField = options.id || "id";
-			var nameField = options.name || "name";
-
-			var idLookup = {};
-			var nameLookup = {};
-			options.data.forEach (function (datum) {
-				var id = datum[idField];
-				var name = datum[nameField];
-				idLookup[id] = datum;
-				nameLookup[name] = datum;
+			Property.apply(this, arguments);
+			$.extend(this, {
+				idField: "id",
+				nameField: "name"
 			});
-
-			var renderer = function (instance, td, row, col, prop, value, cellProperties) {
-				Handsontable.renderers.NumericRenderer.apply(this, [instance, td, row, col, prop, value ? value[nameField] : null, cellProperties]);
-				if (value) {
-					if (!value.id) {
-						td.color = "red";
-					} else {
-						td.title = "ID: " + value.id;
-					}
-				}
-				return td;
-			};
-
-			this.name = function () {
-				return property;
-			};
-			this.get = function (item) {
-				var value = item[property];
-				console.log("Item: ", item, " value: ", value);
-				return value;
-			};
-			this.set = function (item, value) {
-				var ref;
-				if (value === null) {
-					ref = null;
-				} else if (typeof value === 'string') {
-					ref = nameLookup[value];
-					if (!ref) {
-						ref = {};
-						ref[idField] = null;
-						ref[nameField] = value;
-					}					
+			this.idLookup = {};
+			this.nameLookup = {};
+			var self = this;
+			this.data.forEach (function (datum) {
+				var id = datum[self.idField];
+				var name = datum[self.nameField];
+				self.idLookup[id] = datum;
+				self.nameLookup[name] = datum;
+			});
+		};
+		ReferenceProperty.prototype = Object.create(Property.prototype);
+		ReferenceProperty.renderer = function (instance, td, row, col, prop, displayValue, cellProperties) {
+			Handsontable.renderers.NumericRenderer.apply(null, [instance, td, row, col, prop, displayValue, cellProperties]);
+			var value = this.value(row);
+			console.log("Value:", value, "display value:", displayValue);
+			if (value) {
+				if (!value.id) {
+					td.color = "red";
 				} else {
-					ref = value;
+					td.title = "ID: " + value.id;
 				}
-				console.log(">>> SET", item, ref);
-				item[property] = ref;
-			};
-			this.toJson = function (item, json) {
-				json[property] = this.get(item);
-			};
-			this.fromJson = function (item, json) {
-				var value = json[property];
-				this.set(item, value ? idLookup[value] : null);
-			};
-			this.toColumn = function () {
-				return $.extend({}, options.column, {
-					type: "dropdown",
-					source: options.data.map(function (datum) { return datum.name; }),
-					title: options.title,
-					data: makeProperty(this.get, this.set),
-					renderer: renderer
-				});
-			};
-		}
+			}
+			return td;
+		};
+		ReferenceProperty.prototype.get = function (item) {
+			var value = item[this.property];
+			// console.log("Item: ", item, " value: ", value);
+			return value ? value[this.nameField] : null;
+		};
+		ReferenceProperty.prototype.set = function (item, value) {
+			var ref;
+			if (value === null) {
+				ref = null;
+			} else if (typeof value === 'string') {
+				ref = this.nameLookup[value];
+				if (!ref) {
+					ref = {};
+					ref[this.idField] = null;
+					ref[this.nameField] = value;
+				}					
+			} else {
+				ref = value;
+			}
+			// console.log(">>> SET", item, ref);
+			item[this.property] = ref;
+		};
+		ReferenceProperty.prototype.toJson = function (item, json) {
+			json[this.property] = this.get(item);
+		};
+		ReferenceProperty.prototype.fromJson = function (item, json) {
+			var value = json[this.property];
+			this.set(item, value ? this.idLookup[value] : null);
+		};
+		ReferenceProperty.prototype.toColumn = function () {
+			return $.extend({}, this.column, {
+				type: "dropdown",
+				source: this.data.map(function (datum) { return datum.name; }),
+				title: this.title,
+				data: this.toProperty(),
+				renderer: ReferenceProperty.renderer.bind(this)
+			});
+		};
 		
 		var produces = [
 			{ id: 1, name: "Zsázsa", rowWidth: 15 },
@@ -145,7 +152,7 @@
 			{ id: 2, produce: 2, seed: "Mizunamag", time: "2015-05-17", seedsPerGramm: 0.25 },
 		];
 		
-		var idProp = new SimpleProperty({ property: "id", title: "ID" });
+		var idProp = new SimpleProperty({ property: "id", title: "ID", readOnly: true });
 		var produceProp = new ReferenceProperty({ property: "produce", title: "Termény", data: produces });
 		var seedProp = new SimpleProperty({ property: "seed", title: "Mag" });
 		var timeProp = new SimpleProperty({ property: "time", title: "Dátum", type: "date", column: { dateFormat: "YYYY-MM-DD" } });
