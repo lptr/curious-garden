17e273572cc0481337024786349506684f0127d5
Lorant Pinter <lorant.pinter@prezi.com>
2015-05-18 16:39:04 +0200

    Basic persistency

--- modules/planting/planting.js
+++ modules/planting/planting.js
@@ -16,16 +16,6 @@
 	});
 
 	transactionModule.controller("PlantingController", function ($scope, kapaServer) {
-		var produces = [
-			{ id: 1, name: "Zsázsa", rowWidth: 15 },
-			{ id: 2, name: "Mizuna", rowWidth: 12 },
-		];
-		var plantations = [
-			{ id: 1, produce: "Zsázsa", seed: "Zsázsamag", time: "2015-05-13", seedsPerGramm: 4 },
-			{ id: 2, produce: "Mizuna", seed: "Mizunamag", time: "2015-05-17", seedsPerGramm: 0.25 },
-		];
-		$scope.items = plantations;
-
 		var afterChange = function (event) {
 			console.log("Event", event);
 		}
@@ -39,7 +29,7 @@
 			};
 		};
 		
-		var property = function (get, set) {
+		var makeProperty = function (get, set) {
 			return function (item, value) {
 				if (typeof value === 'undefined') {
 					return get.apply({}, [item]);
@@ -49,55 +39,124 @@
 			}
 		}
 		
-		var LookupProperty = function (options) {
-			var nameField = options.name;
-			var idField = options.id || nameField + "Id";
+		var SimpleProperty = function (options) {
+			var property = options.property;
+			
+			this.name = function () {
+				return property;
+			};
+			this.get = function (item) {
+				return item[property];
+			};
+			this.set = function (item, value) {
+				item[property] = value;
+			}
+			this.toJson = function (item, json) {
+				json[property] = this.get(item);
+			};
+			this.fromJson = function (item, json) {
+				this.set(item, json[property]);
+			};
+			this.toColumn = function () {
+				return $.extend({}, options.column, {
+					type: options.type || "text",
+					title: options.title,
+					data: makeProperty(this.get, this.set),
+					readOnly: options.readOnly ? true : false
+				});
+			};
+		};
 
-			var lookup = {};
+		var ReferenceProperty = function (options) {
+			var property = options.property;
+			var idField = options.id || "id";
+			var nameField = options.name || "name";
+
+			var idLookup = {};
+			var nameLookup = {};
 			options.data.forEach (function (datum) {
-				var id = datum["id"];
-				var name = datum["name"];
-				lookup[name] = datum;
+				var id = datum[idField];
+				var name = datum[nameField];
+				idLookup[id] = datum;
+				nameLookup[name] = datum;
 			});
 
-			this.get = function (item) {
-				return item[nameField];
-			};
-			this.set = function (item, name) {
-				item[nameField] = name;
-				var value = lookup[name];
-				item[idField] = value ? value.id : null;
-			};
-			this.renderer = function (instance, td, row, col, prop, name, cellProperties) {
-				Handsontable.renderers.NumericRenderer.apply(this, [instance, td, row, col, prop, name, cellProperties]);
-				var value = lookup[name];
-				if (!value) {
-					td.color = "red";
-				} else {
-					td.title = "ID: " + value.id;
+			var renderer = function (instance, td, row, col, prop, value, cellProperties) {
+				Handsontable.renderers.NumericRenderer.apply(this, [instance, td, row, col, prop, value ? value[nameField] : null, cellProperties]);
+				if (value) {
+					if (!value.id) {
+						td.color = "red";
+					} else {
+						td.title = "ID: " + value.id;
+					}
 				}
 				return td;
 			};
+
+			this.name = function () {
+				return property;
+			};
+			this.get = function (item) {
+				var value = item[property];
+				console.log("Item: ", item, " value: ", value);
+				return value;
+			};
+			this.set = function (item, value) {
+				var ref;
+				if (value === null) {
+					ref = null;
+				} else if (typeof value === 'string') {
+					ref = nameLookup[value];
+					if (!ref) {
+						ref = {};
+						ref[idField] = null;
+						ref[nameField] = value;
+					}					
+				} else {
+					ref = value;
+				}
+				console.log(">>> SET", item, ref);
+				item[property] = ref;
+			};
+			this.toJson = function (item, json) {
+				json[property] = this.get(item);
+			};
+			this.fromJson = function (item, json) {
+				var value = json[property];
+				this.set(item, value ? idLookup[value] : null);
+			};
 			this.toColumn = function () {
-				return {
+				return $.extend({}, options.column, {
 					type: "dropdown",
 					source: options.data.map(function (datum) { return datum.name; }),
 					title: options.title,
-					data: property(this.get, this.set),
-					renderer: this.renderer
-				};
+					data: makeProperty(this.get, this.set),
+					renderer: renderer
+				});
 			};
 		}
 		
-		var produceProp = new LookupProperty({ name: "produce", title: "Termény", data: produces });
-
-		var columns = [
-			{ type: "text", title: "ID", data: "id", readOnly: true },
-			produceProp.toColumn(),
-			{ type: "text", title: "Mag", data: "seed", hidden: true },
-			{ type: "date", title: "Dátum", data: "time", dateFormat: "YYYY-MM-DD" },
-			{ type: "numeric", title: "Magok száma", data: "seedsPerGramm", format: "0.00", renderer: suffixRenderer(" db/g")},
+		var produces = [
+			{ id: 1, name: "Zsázsa", rowWidth: 15 },
+			{ id: 2, name: "Mizuna", rowWidth: 12 },
 		];
+		var plantations = [
+			{ id: 1, produce: 1, seed: "Zsázsamag", time: "2015-05-13", seedsPerGramm: 4 },
+			{ id: 2, produce: 2, seed: "Mizunamag", time: "2015-05-17", seedsPerGramm: 0.25 },
+		];
+		
+		var idProp = new SimpleProperty({ property: "id", title: "ID" });
+		var produceProp = new ReferenceProperty({ property: "produce", title: "Termény", data: produces });
+		var seedProp = new SimpleProperty({ property: "seed", title: "Mag" });
+		var timeProp = new SimpleProperty({ property: "time", title: "Dátum", type: "date", column: { dateFormat: "YYYY-MM-DD" } });
+		var seedCountProp = new SimpleProperty({ property: "seedsPerGramm", title: "Magok száma", type: "numeric", column: { format: "0.00", renderer: suffixRenderer(" db/g") } });
+
+		var properties = [ idProp, produceProp, seedProp, timeProp, seedCountProp ];
+		var dataSchema = {};
+		properties.forEach(function (property) {
+			dataSchema[property] = property.name();
+		});
+		var columns = properties.map(function (property) { return property.toColumn(); });
 
 		$scope.settings = {
 			colHeaders: true,
@@ -107,7 +166,20 @@
 			minSpareRows: 1,
 			height: 300,
 			width: 700,
-			columns: columns.filter(function (column) { return !column.hidden; })
+			columns: columns,
+			dataSchema: dataSchema
 		};
+
+		var reload = function () {
+			$scope.items = [];
+			plantations.forEach(function (plantation) {
+				item = {};
+				properties.forEach(function (property) {
+					return property.fromJson(item, plantation);
+				});
+				$scope.items.push(item);
+			});
+		};
+		reload();
 	});
 })();
