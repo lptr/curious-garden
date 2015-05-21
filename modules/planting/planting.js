4bbe0cfc461c73520aa99a6c274260896cad16cd
Lorant Pinter <lorant.pinter@prezi.com>
2015-05-21 18:02:25 +0200

    Added szinek

--- modules/planting/planting.js
+++ modules/planting/planting.js
@@ -14,7 +14,24 @@
 			});
 	});
 	
-	plantingModule.factory("fajok", function (tables) {
+	plantingModule.factory("Szinek", function (tables) {
+		return new tables.Table({
+			name: "Színek",
+			properties: [
+				{ name: "nev", title: "Szín" },
+				{ name: "name", title: "Color" },
+			],
+			items: {
+				toString: function () { return this.get("nev"); }
+			}
+		});
+	});
+
+	plantingModule.controller("SzinekController", function ($scope, kapaServer, Szinek) {
+		$scope.table = Szinek;
+	});
+	
+	plantingModule.factory("Fajok", function (tables) {
 		return new tables.Table({
 			name: "Fajok",
 			properties: [
@@ -56,8 +73,35 @@
 		});
 	});
 
-	plantingModule.controller("FajokController", function ($scope, kapaServer, fajok) {
-		$scope.table = fajok;
+	plantingModule.controller("FajokController", function ($scope, kapaServer, Fajok) {
+		$scope.table = Fajok;
+	});
+	
+	plantingModule.factory("Magtipusok", function (tables, Fajok, Szinek) {
+		return new tables.Table({
+			name: "Magtípusok",
+			properties: [
+				{
+					name: "nev",
+					title: "Név",
+					width: 240,
+					calculate: function (faj, fajtanev, gyarto) {
+						return _
+							.map(arguments, function (value) { return value.value() ? value.value().toString() : null; })
+							.filter(function (value) { return !!value; })
+							.join(", ");
+					}
+				},
+				new tables.ReferenceProperty({ name: "faj", title: "Faj", target: Fajok, width: 120 }),
+				{ name: "fajtanev", title: "Fajtanév" },
+				{ name: "gyarto", title: "Gyártó" },
+				new tables.ReferenceProperty({ name: "szin", title: "Szín", target: Szinek }),
+			]
+		});
+	});
+
+	plantingModule.controller("MagtipusokController", function ($scope, kapaServer, Magtipusok) {
+		$scope.table = Magtipusok;
 	});
 
 	plantingModule.controller("ChangeTrackingController", function ($scope, changeTracking) {
